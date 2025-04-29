d0a1bc23c7af625940d4cb635549d5412e54fb33
Piyush Kirtivardhan <pkirtivardhan@gmail.com>
2025-04-29 07:24:27 +0530

    Add frontend/src/pages/Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Home = () => {
+  return (
+    <div>
+      <Typography variant="h4">Welcome to Retail Inventory Management</Typography>
+      <Typography variant="body1">Manage your inventory efficiently and effectively.</Typography>
+    </div>
+  );
+};
+
+export default Home;
